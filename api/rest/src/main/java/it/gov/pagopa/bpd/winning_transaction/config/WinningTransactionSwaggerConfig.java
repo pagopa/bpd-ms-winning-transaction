c37b05e0655b8e411ad67fe6c7dd606afebf5374
mdesogus <michael.desogus@everis.com>
2020-05-07 10:27:18 +0200

    fix logging_level properties on application.yml

--- api/rest/src/main/java/it/gov/pagopa/bpd/winning_transaction/config/WinningTransactionSwaggerConfig.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/winning_transaction/config/WinningTransactionSwaggerConfig.java
@@ -9,8 +9,7 @@ public class WinningTransactionSwaggerConfig {
 
     @Configuration
     @Profile("swaggerIT")
-    @PropertySource({"classpath:/swagger/swagger_it_IT.properties",
-            "classpath:/swagger/profile.properties"})
+    @PropertySource("classpath:/swagger/swagger_it_IT.properties")
 
     public static class itConfig {
 
