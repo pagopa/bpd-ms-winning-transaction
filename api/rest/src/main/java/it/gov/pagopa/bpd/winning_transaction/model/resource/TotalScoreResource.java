65449259cf64f87841441bc156719395a16ea712
alecastagno <63311837+alecastagno@users.noreply.github.com>
2020-09-25 11:12:40 +0200

    Fix swagger description (#1)

--- api/rest/src/main/java/it/gov/pagopa/bpd/winning_transaction/model/resource/TotalScoreResource.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/winning_transaction/model/resource/TotalScoreResource.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.bpd.winning_transaction.model.resource;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(of = {"totalScore"}, callSuper = false)
 public class TotalScoreResource {
 
+    @ApiModelProperty(value = "${swagger.winningTransaction.totalScore}", required = true)
     @JsonProperty(required = true)
     private Long totalScore;
 
