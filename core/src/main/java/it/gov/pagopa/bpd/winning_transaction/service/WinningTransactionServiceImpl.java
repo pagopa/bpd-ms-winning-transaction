7363261c4df4e49a0b56d3f78fd1687c4b7b03a1
alecastagno <alessandro.castagno.sia@gmail.com>
2020-11-20 20:02:52 +0100

    Merge release/st

--- core/src/main/java/it/gov/pagopa/bpd/winning_transaction/service/WinningTransactionServiceImpl.java
+++ core/src/main/java/it/gov/pagopa/bpd/winning_transaction/service/WinningTransactionServiceImpl.java
@@ -2,11 +2,9 @@ package it.gov.pagopa.bpd.winning_transaction.service;
 
 import it.gov.pagopa.bpd.winning_transaction.connector.jpa.WinningTransactionDAO;
 import it.gov.pagopa.bpd.winning_transaction.connector.jpa.model.WinningTransaction;
-import it.gov.pagopa.bpd.winning_transaction.connector.jpa.model.WinningTransactionId;
 import it.gov.pagopa.bpd.winning_transaction.exception.WinningTransactionExistsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
@@ -52,7 +50,7 @@ public class WinningTransactionServiceImpl implements WinningTransactionService 
 
         List<WinningTransaction> winningTransactions = new ArrayList<>();
         winningTransactions = hpan != null? winningTransactionDAO.findCitizenTransactionsByHpan(fiscalCode,awardPeriodId,hpan)
-        : winningTransactionDAO.findCitizenTransactions(fiscalCode, awardPeriodId);
+                : winningTransactionDAO.findCitizenTransactions(fiscalCode, awardPeriodId);
 
         return winningTransactions;
     }
